53985db0f838e410f07d9781caebb520fc237bb1
Michael Rader <mrader1248@gmail.com>
2024-02-25 15:49:50 +0100

    added asset allocation sunburst plot

--- gnucash_tools/dashboard.py
+++ gnucash_tools/dashboard.py
@@ -6,12 +6,37 @@ from dash import Dash, dcc, html
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from .common import Book
+from .common.account import Account
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     gnucash_file: str
     assets_account_name: str
+
+
+def create_asset_allocation_sunburst(account: Account) -> go.Sunburst:
+    def get_labels_parents_values(account: Account) -> tuple[list, list, list]:
+        labels = [account.name]
+        parents = [account.parent_account.name]
+        values = [account.total_balance_history.values[-1]]
+        for child_account in account.child_accounts:
+            labels_to_add, parents_to_add, values_to_add = get_labels_parents_values(
+                child_account
+            )
+            labels += labels_to_add
+            parents += parents_to_add
+            values += values_to_add
+        return labels, parents, values
+
+    labels, parents, values = get_labels_parents_values(account)
+    return go.Sunburst(
+        labels=labels[1:],
+        parents=parents[1:],
+        values=values[1:],
+        branchvalues="total",
+        textinfo="label+percent entry",
+    )
 
 
 @click.command()
@@ -27,14 +52,32 @@ def main(**kwargs) -> None:
     assets_balance_history = assets_account.total_balance_history
 
     app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG, dbc.icons.BOOTSTRAP])
-    figure = go.Figure(
+
+    assets_over_time_figure = go.Figure(
         go.Scatter(
             x=assets_balance_history.dates,
             y=assets_balance_history.values,
         )
     )
-    figure.update_layout(template="plotly_dark", title="Vermögen")
-    app.layout = dbc.Container(
-        [dbc.Row([dbc.Col([dcc.Graph(figure=figure)])])],
+    assets_over_time_figure.update_layout(template="plotly_dark", title="Assets")
+
+    asset_allocation_figure = go.Figure(
+        create_asset_allocation_sunburst(assets_account)
+    )
+    asset_allocation_figure.update_layout(
+        template="plotly_dark",
+        title="Asset allocation",
+        margin=dict(t=0, l=0, r=0, b=0),
+    )
+
+    app.layout = html.Div(
+        [
+            dbc.Row(
+                [
+                    dbc.Col(dcc.Graph(figure=assets_over_time_figure)),
+                    dbc.Col(dcc.Graph(figure=asset_allocation_figure), width=4),
+                ]
+            )
+        ],
     )
     app.run(debug=True)
