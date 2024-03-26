a3d04a9af08baf203f612c9fe02f39cb90030a22
Michael Rader <mrader1248@gmail.com>
2024-03-26 17:18:03 +0100

    unified sunburst chart creation

--- gnucash_tools/dashboard.py
+++ gnucash_tools/dashboard.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 from functools import reduce
+from typing import Callable
 
 import click
 import dash_bootstrap_components as dbc
@@ -15,32 +17,46 @@ class Settings(BaseSettings):
     gnucash_file: str
 
 
-def create_asset_allocation_sunburst(book: Book) -> go.Sunburst:
-    asset_accounts = [
-        account
-        for account in book.accounts
-        if account.type not in {AccountType.INCOME, AccountType.EXPENSE}
-        and not account.is_root_account
-        and len(total_balance_history := account.total_balance_history) > 0
-        and total_balance_history.values[-1] > 0
-    ]
-    labels = [account.name for account in asset_accounts]
+def create_sunburst(
+    book: Book,
+    select_account: Callable[Account, bool],
+    get_value_from_account: Callable[Account, Decimal],
+    return_total_value=False,
+    additional_slice: tuple[str, Decimal] = None,
+    maxdepth=3,
+) -> go.Sunburst:
+    accounts = [account for account in book.accounts if select_account(account)]
+    labels = [account.name for account in accounts]
     parents = [
         (account.parent_account.name if account.parent_account_id is not None else "")
-        for account in asset_accounts
+        for account in accounts
     ]
-    values = [account.total_balance_history.values[-1] for account in asset_accounts]
+    values = [get_value_from_account(account) for account in accounts]
 
-    return go.Sunburst(
+    root_index = next(j for j in range(len(accounts)) if parents[j] not in labels)
+
+    if additional_slice is not None:
+        additional_slice_label, additional_slice_value = additional_slice
+        values[root_index] += additional_slice_value
+        labels.append(additional_slice_label)
+        parents.append(labels[root_index])
+        values.append(additional_slice_value)
+
+    sunburst = go.Sunburst(
         labels=labels[1:],
         parents=parents[1:],
         values=values[1:],
         branchvalues="total",
         textinfo="label+percent entry+value",
         hoverinfo="label+percent entry+value",
-        maxdepth=3,
+        maxdepth=maxdepth,
         insidetextorientation="horizontal",
     )
+    if return_total_value:
+        total_value = values[root_index]
+        return sunburst, total_value
+    else:
+        return sunburst
 
 
 def create_equity_over_time_plot(book: Book):
@@ -69,18 +85,66 @@ def main(**kwargs) -> None:
     app = Dash(
         __name__,
         external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP],
-        # external_stylesheets=[dbc.themes.CYBORG, dbc.icons.BOOTSTRAP]
+        # external_stylesheets=[dbc.themes.CYBORG, dbc.icons.BOOTSTRAP],
     )
     FIGURE_TEMPLATE = "plotly_white"
+    # FIGURE_TEMPLATE = "plotly_dark"
 
     equity_over_time_figure = go.Figure(create_equity_over_time_plot(book))
     equity_over_time_figure.update_layout(
         template=FIGURE_TEMPLATE,
-        yaxis_title="Equity",
         margin=dict(t=0, l=0, r=0, b=0),
     )
 
-    asset_allocation_figure = go.Figure(create_asset_allocation_sunburst(book))
+    cashflow_in_sunburst, cashflow_in_total = create_sunburst(
+        book,
+        lambda account: account.type == AccountType.INCOME,
+        lambda account: -account.total_balance_history.values[-1],
+        return_total_value=True,
+        maxdepth=3,
+    )
+    cashflow_out_sunburst, cashflow_out_total = create_sunburst(
+        book,
+        lambda account: account.type == AccountType.EXPENSE,
+        lambda account: account.total_balance_history.values[-1],
+        return_total_value=True,
+        maxdepth=3,
+    )
+    if cashflow_in_total > cashflow_out_total:
+        cashflow_out_sunburst = create_sunburst(
+            book,
+            lambda account: account.type == AccountType.EXPENSE,
+            lambda account: account.total_balance_history.values[-1],
+            additional_slice=("Savings", cashflow_in_total - cashflow_out_total),
+            maxdepth=3,
+        )
+    else:
+        raise NotImplementedError()
+
+    cashflow_in_figure = go.Figure(cashflow_in_sunburst)
+    cashflow_in_figure.update_layout(
+        template=FIGURE_TEMPLATE,
+        margin=dict(t=0, l=0, r=0, b=0),
+    )
+
+    cashflow_out_figure = go.Figure(cashflow_out_sunburst)
+    cashflow_out_figure.update_layout(
+        template=FIGURE_TEMPLATE,
+        margin=dict(t=0, l=0, r=0, b=0),
+    )
+
+    asset_allocation_figure = go.Figure(
+        create_sunburst(
+            book,
+            lambda account: (
+                account.type not in {AccountType.INCOME, AccountType.EXPENSE}
+                and not account.is_root_account
+                and len(total_balance_history := account.total_balance_history) > 0
+                and total_balance_history.values[-1] > 0
+            ),
+            lambda account: account.total_balance_history.values[-1],
+        )
+    )
     asset_allocation_figure.update_layout(
         template=FIGURE_TEMPLATE,
         title="Asset allocation",
@@ -89,12 +153,31 @@ def main(**kwargs) -> None:
 
     app.layout = dbc.Container(  # html.Div(
         [
+            dbc.Row([dbc.Col([html.H2("Equity"), html.Br()])]),
             dbc.Row(
                 [
                     dbc.Col(dcc.Graph(figure=equity_over_time_figure)),
-                    dbc.Col(dcc.Graph(figure=asset_allocation_figure), width=4),
                 ]
-            )
+            ),
+            dbc.Row([dbc.Col([html.Br(), html.Br(), html.H2("Cashflow"), html.Br()])]),
+            dbc.Row(
+                [
+                    dbc.Col(dcc.Graph(figure=cashflow_in_figure), width=6),
+                    dbc.Col(dcc.Graph(figure=cashflow_out_figure), width=6),
+                ]
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [html.Br(), html.Br(), html.H2("Asset allocation"), html.Br()]
+                    )
+                ]
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(dcc.Graph(figure=asset_allocation_figure), width=6),
+                ]
+            ),
         ],
     )
     app.run(debug=True)
